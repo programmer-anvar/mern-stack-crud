7f64f6292ece5ccef066b177a9881cc42c7374f3
choi won suk <hamidulloxon@gmail.com>
2025-06-19 12:56:24 +0500

    add chakra ui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Button, Container, Flex, HStack, Link, Text,ClientOnly, IconButton, Skeleton  } from '@chakra-ui/react'
 import React from 'react'
+import { useColorMode } from "./ui/color-mode"
+import { LuMoon, LuSun } from "react-icons/lu"
+
+
 
 const Navbar = () => {
+  const { toggleColorMode, colorMode } = useColorMode()
   return (
-    <Container maxW={'1140px'}>
+    <Container maxW={'1140px'} px={4} >
      <Flex h={'16'}
       alignItems={'center'} 
       justifyContent={'space-between'}
@@ -12,7 +17,33 @@ const Navbar = () => {
         md:'row'
       }}
       >
+       <Text 
+       fontSize={{base:'22',sm:'28'}}
+      //  fontWeight={bold}
+       textTransform={'uppercase'}
+       textAlign={'center'}
+       bgGradient={'linear(to-r),cyn.400, blue.500'}
+       bgClip={"text"}
+       >
+        <Link to={'/'}>Prodact store</Link>
+
        
+
+       </Text>
+
+       <HStack spacing={2} alignItems={'center'}>
+            <Link>
+            <Button>
+              +
+              {/* <FaPlus/> */}
+            </Button>
+            </Link>
+            <ClientOnly fallback={<Skeleton boxSize="8" />}>
+      <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+        {colorMode === "light" ? <LuSun /> : <LuMoon />}
+      </IconButton>
+    </ClientOnly>
+        </HStack>
      </Flex>
     </Container>
   )
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import { Box, Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useColorModeValue } from '../components/ui/color-mode'
 
 const CreatePage = () => {
+  const [newProduct,setNewProduct] = useState({
+    name:"",
+    price:"",
+    image:""
+  })
+
+  const handleAddProduct = () =>{
+    console.log(newProduct);
+    
+  }
   return (
-    <div>
-      
-    </div>
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={'center'} mb={'8'}>
+           Create Newv Product
+        </Heading>
+         <Box w={'full'} bg={useColorModeValue("white","gray.800")}
+          p={6} rounded={'lg'} shadow={'md'}
+         >
+           <VStack spacing={4}>
+            <Input
+             placeholder='Product Name'
+             name='name'
+             value={newProduct.name}
+             onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
+            />
+
+            <Input
+             placeholder='Product Price'
+             name='price'
+             value={newProduct.price}
+             onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}
+            />
+
+            <Input
+             placeholder='Product Image'
+             name='image'
+             value={newProduct.image}
+             onChange={(e) => setNewProduct({...newProduct, image: e.target.value})}
+            />
+
+            <Button w={'full'} colorScheme={'blue'} onClick={handleAddProduct}>Add Product</Button>
+           </VStack>
+         </Box>
+      </VStack>
+    </Container>
   )
 }
 
